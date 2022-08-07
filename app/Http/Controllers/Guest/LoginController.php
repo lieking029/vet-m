14d69da04a09a1102ee710c36e2bdf0d58116794
Dan Janus Pineda <pinedadanjanus@gmail.com>
2022-08-07 15:14:00 +0800

    revert changes

--- app/Http/Controllers/Guest/LoginController.php
+++ app/Http/Controllers/Guest/LoginController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Guest;
 use App\Exceptions\VerifyEmailException;
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
+    use AuthenticateUsers;
     /**
      * Create a new controller instance.
      *
@@ -20,11 +22,10 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function directLogin(Request $request)
-    {
-        if ($token = $this->login($request)) {
+    public function directLogin(Request $request){
+        if($token = $this->login($request)){
             $token = json_decode(json_encode($token));
-            return redirect()->to('/verify-redirect?token=' . $token->original->token . '&expires=' . $token->original->expires_in);
+            return redirect()->to('/verify-redirect?token='. $token->original->token . '&expires=' . $token->original->expires_in);
         }
         return abort(404);
     }
@@ -32,37 +33,38 @@ class LoginController extends Controller
     /**
      * Attempt to log the user into the application.
      *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
      */
     protected function attemptLogin(Request $request)
     {
-        if (!auth()->attempt($request->all())) {
-            abort(403);
+        $token = $this->guard()->attempt($this->credentials($request));
+
+        if (! $token) {
+            return false;
         }
 
-        $user = auth()->user();
-        if ($user instanceof MustVerifyEmail && !$user->hasVerifiedEmail()) {
-            return response()->json(['message' => 'Verify your account'], 403);
+        $user = $this->guard()->user();
+        if ($user instanceof MustVerifyEmail && ! $user->hasVerifiedEmail()) {
+            return false;
         }
 
-        return response()->json([
-            'remember' => true,
-            'token' => $user->createToken($request->ip())->plainTextToken,
-        ]);
+        $this->guard()->setToken($token);
+
+        return true;
     }
 
     /**
      * Send the response after the user was authenticated.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\JsonResponse
      */
     protected function sendLoginResponse(Request $request)
     {
         $this->clearLoginAttempts($request);
 
-        $token = (string)$this->guard()->getToken();
+        $token = (string) $this->guard()->getToken();
         $expiration = $this->guard()->getPayload()->get('exp');
 
         return response()->json([
@@ -75,7 +77,7 @@ class LoginController extends Controller
     /**
      * Get the failed login response instance.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\JsonResponse
      *
      * @throws \Illuminate\Validation\ValidationException
@@ -83,7 +85,7 @@ class LoginController extends Controller
     protected function sendFailedLoginResponse(Request $request)
     {
         $user = $this->guard()->user();
-        if ($user instanceof MustVerifyEmail && !$user->hasVerifiedEmail()) {
+        if ($user instanceof MustVerifyEmail && ! $user->hasVerifiedEmail()) {
             throw VerifyEmailException::forUser($user);
         }
 
@@ -95,13 +97,15 @@ class LoginController extends Controller
     /**
      * Log the user out of the application.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function logout(Request $request)
     {
         $this->guard()->logout();
     }
+
+
 
 
 }
