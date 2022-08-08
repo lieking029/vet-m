585b4a187f450b4265498775240f1f66a4d248be
Dan Janus Pineda <pinedadanjanus@gmail.com>
2022-08-08 14:52:49 +0800

    enabled dev tools

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,25 @@
-  
+
 
 // window.Vue = require('vue');
- 
+
 import 'vuetify/dist/vuetify.min.css'
 import Vue from 'vue';
-import store from '~/store' 
-import router from '~/router' 
-import vuetify from '~/plugins/vuetify' 
+import store from '~/store'
+import router from '~/router'
+import vuetify from '~/plugins/vuetify'
 import App from '~/Components/App'
 import Vuetify from 'vuetify'
 
 import '~/plugins'
-import '~/Components' 
+import '~/Components'
 window.moment = require('moment');
-  
+
 Vue.use(Vuetify)
+Vue.config.devtools = true;
 new Vue({
     el: '#app',
-    vuetify, 
+    vuetify,
     store,
     router,
     ...App
   })
- 
